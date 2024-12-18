c71d115147e50453a7a4edc427902b0bfa75b00a
Joren Dumoulin <joren.dumoulin@kuleuven.be>
2024-12-18 11:46:26 +0100

    add all mlir core dialects

--- xdsl_aie/dialects/__init__.py
+++ xdsl_aie/dialects/__init__.py
@@ -6,6 +6,11 @@ from xdsl.ir import Dialect
 def get_all_dialects() -> dict[str, Callable[[], Dialect]]:
     """Returns all available dialects."""
 
+    def get_affine():
+        from xdsl.dialects.affine import Affine
+
+        return Affine
+
     def get_aie():
         from xdsl_aie.dialects.aie import AIE
 
@@ -16,25 +21,55 @@ def get_all_dialects() -> dict[str, Callable[[], Dialect]]:
 
         return Arith
 
+    def get_bufferization():
+        from xdsl.dialects.bufferization import Bufferization
+
+        return Bufferization
+
     def get_builtin():
         from xdsl.dialects.builtin import Builtin
 
         return Builtin
+
+    def get_cf():
+        from xdsl.dialects.cf import Cf
+
+        return Cf
 
     def get_func():
         from xdsl.dialects.func import Func
 
         return Func
 
+    def get_gpu():
+        from xdsl.dialects.gpu import GPU
+
+        return GPU
+
     def get_linalg():
         from xdsl.dialects.linalg import Linalg
 
         return Linalg
 
+    def get_llvm():
+        from xdsl.dialects.llvm import LLVM
+
+        return LLVM
+
     def get_memref():
         from xdsl.dialects.memref import MemRef
 
         return MemRef
+
+    def get_mpi():
+        from xdsl.dialects.mpi import MPI
+
+        return MPI
+
+    def get_pdl():
+        from xdsl.dialects.pdl import PDL
+
+        return PDL
 
     def get_scf():
         from xdsl.dialects.scf import Scf
@@ -51,14 +86,27 @@ def get_all_dialects() -> dict[str, Callable[[], Dialect]]:
 
         return Test
 
+    def get_vector():
+        from xdsl.dialects.vector import Vector
+
+        return Vector
+
     return {
+        "affine": get_affine,
         "aie": get_aie,
         "arith": get_arith,
+        "bufferization": get_bufferization,
         "builtin": get_builtin,
+        "cf": get_cf,
         "func": get_func,
+        "gpu": get_gpu,
         "linalg": get_linalg,
+        "llvm": get_llvm,
         "memref": get_memref,
+        "mpi": get_mpi,
+        "pdl": get_pdl,
         "scf": get_scf,
         "tensor": get_tensor,
         "test": get_test,
+        "vector": get_vector,
     }
