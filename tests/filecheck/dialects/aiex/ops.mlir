67268f621c21305e56dacad0068b48c6b86e43ee
Joren Dumoulin <joren.dumoulin@kuleuven.be>
2025-01-08 16:41:43 +0100

    add aiex.npu.dma_memcpy_nd operation

--- tests/filecheck/dialects/aiex/ops.mlir
+++ tests/filecheck/dialects/aiex/ops.mlir
@@ -35,4 +35,28 @@ aiex.runtime_sequence @testabc(%2 : memref<16xi8>, %3 : memref<16xi8>) { }
 // CHECK-GENERIC-NEXT: ^{{.*}}(%{{.*}}: memref<16xi8>, %{{.*}}: memref<16xi8>):
 // CHECK-GENERIC-NEXT: }) : () -> ()
 
+
+aiex.runtime_sequence (%4 : memref<4096xi8>) {
+
+// CHECK-NEXT: aiex.runtime_sequence(%{{.*}} : memref<4096xi8>) {
+// CHECK-GENERIC-NEXT: "aiex.runtime_sequence"() ({
+// CHECK-GENERIC-NEXT: ^2(%{{.*}} : memref<4096xi8>):
+
+aiex.npu.dma_memcpy_nd(0, 0, %4[0, 0, 0, 0][1, 1, 1, 4096][0, 0, 0, 1]) {id = 2 : i64, issue_token = true, metadata = @test} : memref<4096xi8>
+
+// CHECK-NEXT: aiex.npu.dma_memcpy_nd(0, 0, %{{.*}}[0, 0, 0, 0][1, 1, 1, 4096][0, 0, 0, 1]) {id = 2 : i64, issue_token = true, metadata = @test} : memref<4096xi8>
+
+// CHECK-GENERIC-NEXT: "aiex.npu.dma_memcpy_nd"(%{{.*}}) <{
+// CHECK-GENERIC-SAME: id = 2 : i64
+// CHECK-GENERIC-SAME: issue_token = true
+// CHECK-GENERIC-SAME: metadata = @test
+// CHECK-GENERIC-SAME: static_offsets = array<i64: 0, 0, 0, 0> 
+// CHECK-GENERIC-SAME: static_sizes = array<i64: 1, 1, 1, 4096> 
+// CHECK-GENERIC-SAME: static_strides = array<i64: 0, 0, 0, 1>
+// CHECK-GENERIC-SAME: x = 0 : i64
+// CHECK-GENERIC-SAME: y = 0 : i64 
+// CHECK-GENERIC-SAME: }> : (memref<4096xi8>) -> ()
+
+}
+
 }
--- xdsl_aie/dialects/aiex.py
+++ xdsl_aie/dialects/aiex.py
@@ -1,18 +1,163 @@
+from collections.abc import Sequence
+from typing import cast
+
 from typing_extensions import Self
-from xdsl.dialects.builtin import StringAttr
-from xdsl.ir import Dialect, Region
+from xdsl.dialects.builtin import (
+    BoolAttr,
+    DenseArrayBase,
+    IntegerAttr,
+    IntegerType,
+    MemRefType,
+    StringAttr,
+    SymbolRefAttr,
+    i64,
+)
+from xdsl.ir import Dialect, Operation, Region, SSAValue
 from xdsl.irdl import (
+    AttrSizedOperandSegments,
     IRDLOperation,
     irdl_op_definition,
+    operand_def,
     opt_prop_def,
+    prop_def,
     region_def,
     traits_def,
+    var_operand_def,
 )
 from xdsl.parser import Parser
 from xdsl.printer import Printer
 from xdsl.traits import HasParent, NoTerminator
+from xdsl.utils.hints import isa
 
 from xdsl_aie.dialects.aie import DeviceOp
+
+
+@irdl_op_definition
+class DmaMemcpyNdOp(IRDLOperation):
+    name = "aiex.npu.dma_memcpy_nd"
+
+    memref = operand_def(MemRefType)
+
+    offsets = var_operand_def(IntegerType)
+    sizes = var_operand_def(IntegerType)
+    strides = var_operand_def(IntegerType)
+
+    id = prop_def(IntegerAttr[IntegerType])
+    issue_token = prop_def(BoolAttr)
+    metadata = prop_def(SymbolRefAttr)
+
+    irdl_options = [AttrSizedOperandSegments(as_property=True)]
+
+    static_offsets = prop_def(DenseArrayBase)
+    static_sizes = prop_def(DenseArrayBase)
+    static_strides = prop_def(DenseArrayBase)
+
+    x = prop_def(IntegerAttr[IntegerType])
+    y = prop_def(IntegerAttr[IntegerType])
+
+
+    def __init__(
+        self,
+        x: int | IntegerAttr[IntegerType],
+        y: int | IntegerAttr[IntegerType],
+        memref: SSAValue | Operation,
+        static_offsets: Sequence[int] | DenseArrayBase,
+        static_sizes: Sequence[int] | DenseArrayBase,
+        static_strides: Sequence[int] | DenseArrayBase,
+        metadata: str | StringAttr | SymbolRefAttr,
+        id: int | IntegerAttr[IntegerType],
+        issue_token: bool | BoolAttr,
+        offsets: Sequence[SSAValue | Operation] = [],
+        sizes: Sequence[SSAValue | Operation] = [],
+        strides: Sequence[SSAValue | Operation] = [],
+    ):
+        if isinstance(x, int):
+            x = IntegerAttr(x, i64)
+        if isinstance(y, int):
+            y = IntegerAttr(y, i64)
+        if isa(static_offsets, Sequence[int]):
+            static_offsets = DenseArrayBase.from_list(i64, static_offsets)
+        static_offsets = cast(DenseArrayBase, static_offsets)
+        if isa(static_sizes, Sequence[int]):
+            static_sizes = DenseArrayBase.from_list(i64, static_sizes)
+        static_sizes = cast(DenseArrayBase, static_sizes)
+        if isa(static_strides, Sequence[int]):
+            static_strides = DenseArrayBase.from_list(i64, static_strides)
+        static_strides = cast(DenseArrayBase, static_strides)
+        if not isinstance(metadata, SymbolRefAttr):
+            metadata = SymbolRefAttr(metadata)
+        if isinstance(id, int):
+            id = IntegerAttr(id, i64)
+        if isinstance(issue_token, bool):
+            issue_token = IntegerAttr.from_bool(issue_token)
+
+        super().__init__(
+            operands=[memref, offsets, sizes, strides],
+            properties={
+                "id": id,
+                "issue_token": issue_token,
+                "metadata": metadata,
+                "static_offsets": static_offsets,
+                "static_sizes": static_sizes,
+                "static_strides": static_strides,
+                "x": x,
+                "y": y,
+            },
+        )
+
+        pass
+
+    def print(self, printer: Printer):
+        printer.print_string(f"({self.x.value.data}, {self.y.value.data}, ")
+        printer.print_operand(self.memref)
+        for static_operands in (self.static_offsets, self.static_sizes, self.static_strides):
+            values = ', '.join([str(x) for x in static_operands.get_values()])
+            printer.print_string(f"[{values}]")
+        for other_operands in (self.offsets, self.sizes, self.strides):
+            if other_operands:
+                printer.print_string(", ")
+                printer.print_operands(other_operands)
+        printer.print_string(") ")
+        printer.print_attr_dict({"id": self.id, "issue_token": self.issue_token, "metadata": self.metadata})
+        printer.print_string(" : ")
+        printer.print_attribute(self.memref.type) 
+
+    @classmethod
+    def parse(cls, parser: Parser) -> Self:
+        parser.parse_punctuation("(")
+        x = parser.parse_integer()
+        parser.parse_punctuation(",")
+        y = parser.parse_integer()
+        parser.parse_punctuation(",")
+        memref = parser.parse_operand()
+        static_offsets = parser.parse_comma_separated_list(
+            parser.Delimiter.SQUARE, parser.parse_integer
+        )
+        static_sizes = parser.parse_comma_separated_list(
+            parser.Delimiter.SQUARE, parser.parse_integer
+        )
+        static_strides = parser.parse_comma_separated_list(
+            parser.Delimiter.SQUARE, parser.parse_integer
+        )
+        parser.parse_punctuation(")")
+        attrs = parser.parse_optional_dictionary_attr_dict()
+        assert isinstance(metadata := attrs["metadata"], SymbolRefAttr)
+        assert isa(id := attrs["id"], IntegerAttr[IntegerType])
+        assert isa(issue_token := attrs["issue_token"], BoolAttr)
+        parser.parse_punctuation(":")
+        parser.parse_type()  # memref type
+
+        return cls(
+            x,
+            y,
+            memref,
+            static_offsets,
+            static_sizes,
+            static_strides,
+            metadata,
+            id,
+            issue_token,
+        )
 
 
 @irdl_op_definition
@@ -62,6 +207,7 @@ class RuntimeSequenceOp(IRDLOperation):
 AIEX = Dialect(
     "aiex",
     [
+        DmaMemcpyNdOp,
         RuntimeSequenceOp,
     ],
     [],
